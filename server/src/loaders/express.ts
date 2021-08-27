28fdf9f29294d4b73b97f78af058f119777f383e
kangjietan <kangjietan@gmail.com>
2021-08-26 22:16:25 -0700

    Update express loader to bodyParser and routes

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -1,9 +1,14 @@
-import { Application } from "express";
+import { Application, urlencoded, json } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import config from "@/config";
+import routes from "@/api";
 
 export default ({ app }: { app: Application }) => {
   app.enable("trust proxy");
+  app.use(urlencoded({ extended: false }));
+  app.use(json());
   app.use(cors());
-  app.use(morgan("tiny"));
+  app.use(morgan("dev"));
+  app.use(config.api.prefix, routes());
 };
