14391a02cc0cc90a30763303c741d16e70aba68d
kangjietan <kangjietan@gmail.com>
2021-09-16 19:03:26 -0700

    Use mongoose loader

--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -1,5 +1,8 @@
 import expressLoader from "./express";
+import mongooseLoader from "./mongoose";
 
 export default async ({ expressApp }) => {
+  await mongooseLoader();
+  console.log("Connected to database");
   await expressLoader({ app: expressApp });
 };
