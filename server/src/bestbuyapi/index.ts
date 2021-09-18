8a046455ab56d30e557940aa940df02c9e5d8c58
kangjietan <kangjietan@gmail.com>
2021-09-18 01:49:12 -0700

    Best Buy Api helper functions

---
+++ server/src/bestbuyapi/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import config from "../config";
+import { BestBuyProduct as BestBuyProductInterface } from "@/interfaces";
+
+const productsBaseUrl = "https://api.bestbuy.com/v1/products";
+const apiKey = config.bestBuyKey;
+const responseFormat = "&format=json";
+
+export const getProduct = async (sku) => {
+  const getProductsUrl = `${productsBaseUrl}(sku=${sku})?apiKey=${apiKey}${responseFormat}`;
+  try {
+    const response = await axios.get(getProductsUrl);
+    const products: BestBuyProductInterface[] = response.data.products;
+
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].sku === sku) return products[i];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
