ef85bc69618c81ae8ad09839f94b08d0a6702d2f
kangjietan <kangjietan@gmail.com>
2021-09-20 23:29:12 -0700

    Start job loaders

--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -1,8 +1,12 @@
 import expressLoader from "./express";
 import mongooseLoader from "./mongoose";
+import jobsLoader from "./jobs";
 
 export default async ({ expressApp }) => {
   await mongooseLoader();
   console.log("Connected to database");
   await expressLoader({ app: expressApp });
+  console.log("Server started");
+  await jobsLoader();
+  console.log("Jobs scheduled");
 };
