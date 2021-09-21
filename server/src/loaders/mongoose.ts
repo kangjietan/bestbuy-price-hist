809d1a261aa9f41287930f3920a7a1969ef18ae6
kangjietan <kangjietan@gmail.com>
2021-09-20 23:51:42 -0700

    Change to arrow function

--- server/src/loaders/mongoose.ts
+++ server/src/loaders/mongoose.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import config from "../config";
 
-export default async function () {
+export default async () => {
   const connection = await mongoose.connect(config.databaseURL);
   return connection.connection.db;
-}
+};
