beae40cdae4b2831fd68e0a37861cb4738153206
kangjietan <kangjietan@gmail.com>
2021-09-21 21:14:30 -0700

    Remove request limiter and refactor to Best Buy API folder

--- server/src/jobs/updatePrices.ts
+++ server/src/jobs/updatePrices.ts
@@ -1,11 +1,10 @@
 import cron from "node-cron";
-import { RateLimiter } from "limiter";
 import { fetchAllItems, updateItemHistoricals } from "../services/item";
 import { addPriceRecord } from "../services/price";
 import { getProduct } from "../bestbuyapi";
 import { Item as ItemInterface } from "../interfaces";
 
-export default async function updateItemPrices() {
+const updateItemPrices = async () => {
   const updatePricesTask = cron.schedule("0 */1 * * *", async () => {
     console.log(`Running every hour. ${new Date()}`);
     fetchItemsAndUpdate();
@@ -14,66 +13,68 @@ export default async function updateItemPrices() {
   await updatePricesTask.start();
 
   return updatePricesTask;
-}
+};
 
-async function fetchItemsAndUpdate() {
-  const limiter = new RateLimiter({
-    tokensPerInterval: 4,
-    interval: "second",
-  });
+export const fetchItemsAndUpdate = async () => {
+  try {
+    const items = await fetchAllItems();
 
-  const items = await fetchAllItems();
-
-  for (let i = 0; i < items.length; i++) {
-    const remainingRequests = await limiter.removeTokens(1);
-    comparePricesAndUpdate(items[i]);
+    for (let i = 0; i < items.length; i++) {
+      comparePricesAndUpdate(items[i]);
+    }
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
-async function comparePricesAndUpdate(item: ItemInterface) {
-  const {
-    currentPrice,
-    priceUpdatedAt,
-    sku,
-    historicalLowPrice,
-    historicalHighPrice,
-  } = item;
+const comparePricesAndUpdate = async (item: ItemInterface) => {
+  try {
+    const {
+      currentPrice,
+      priceUpdatedAt,
+      sku,
+      historicalLowPrice,
+      historicalHighPrice,
+    } = item;
 
-  const productInfo = await getProduct(sku);
-  const { salePrice, priceUpdateDate } = productInfo;
+    const productInfo = await getProduct(sku);
+    const { salePrice, priceUpdateDate } = productInfo;
 
-  if (
-    salePrice === currentPrice ||
-    priceUpdatedAt.getTime() === new Date(priceUpdateDate).getTime()
-  )
-    return;
+    console.log(sku);
 
-  const priceRecord = await addPriceRecord({
-    itemSku: sku,
-    price: salePrice,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  });
+    if (salePrice === currentPrice) return;
 
-  /**
-   * Update historical lows and highs if the current retrieved price is:
-   * Lower than historicalLow
-   * Higher than historicalHigh
-   */
-
-  if (historicalLowPrice < salePrice) {
-    updateItemHistoricals(sku, {
-      historicalLowPrice: salePrice,
-      historicalLowPriceDate: new Date(priceUpdateDate),
+    const priceRecord = await addPriceRecord(sku, {
+      itemSku: sku,
+      price: salePrice,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     });
-  }
 
-  if (historicalHighPrice > salePrice) {
-    updateItemHistoricals(sku, {
-      historicalHighPrice: salePrice,
-      historicalHighPriceDate: new Date(priceUpdateDate),
-    });
-  }
+    /**
+     * Update historical lows and highs if the current retrieved price is:
+     * Lower than historicalLow
+     * Higher than historicalHigh
+     */
 
-  return priceRecord;
-}
+    if (historicalLowPrice < salePrice) {
+      updateItemHistoricals(sku, {
+        historicalLowPrice: salePrice,
+        historicalLowPriceDate: new Date(priceUpdateDate),
+      });
+    }
+
+    if (historicalHighPrice > salePrice) {
+      updateItemHistoricals(sku, {
+        historicalHighPrice: salePrice,
+        historicalHighPriceDate: new Date(priceUpdateDate),
+      });
+    }
+
+    return priceRecord;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default updateItemPrices;
