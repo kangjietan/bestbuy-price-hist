ac2bd6b530155222c339dd0675411d70b5fc49b6
kangjietan <kangjietan@gmail.com>
2021-09-21 21:15:04 -0700

    Add rate limiter to 3 requests per second to Best Buy API

--- server/src/bestbuyapi/index.ts
+++ server/src/bestbuyapi/index.ts
@@ -1,14 +1,22 @@
 import axios from "axios";
 import config from "../config";
+import { RateLimiter } from "limiter";
 import { BestBuyProduct as BestBuyProductInterface } from "../interfaces";
 
 const productsBaseUrl = "https://api.bestbuy.com/v1/products";
 const apiKey = config.bestBuyKey;
 const responseFormat = "&format=json";
 
+const limiter = new RateLimiter({
+  tokensPerInterval: 3,
+  interval: "second",
+});
+
 export const getProduct = async (sku: number) => {
   const getProductsUrl = `${productsBaseUrl}(sku=${sku})?apiKey=${apiKey}${responseFormat}`;
   try {
+    const remainingRequests = await limiter.removeTokens(1);
+
     const response = await axios.get(getProductsUrl);
     const products: BestBuyProductInterface[] = response.data.products;
 
