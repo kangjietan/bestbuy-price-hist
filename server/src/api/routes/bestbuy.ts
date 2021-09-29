e5d171e9aa41fb3d84ce1eb023d69207199c732d
kangjietan <kangjietan@gmail.com>
2021-09-28 20:54:13 -0700

    Add route to retrieve product info from Best Buy API

--- server/src/api/routes/bestbuy.ts
+++ server/src/api/routes/bestbuy.ts
@@ -1,9 +1,28 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+import createError from "http-errors";
+import { fetchProduct } from "../../bestbuyapi";
 
 const route = Router();
 
 export default (app: Router) => {
-  app.use('/bestbuy', route);
+  app.use("/bestbuy", route);
 
-  
-}
+  route.get(
+    "/product/:sku",
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const { sku } = req.params;
+        
+        const product = await fetchProduct(Number(sku));
+
+        if (!product) {
+          next(createError(404, "No product with sku found."));
+        }
+
+        res.json(product);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+};
