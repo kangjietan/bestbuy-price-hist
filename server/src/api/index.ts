2f2938f06752c7c98213519309e51ac7f216d3cf
kangjietan <kangjietan@gmail.com>
2021-09-28 23:02:24 -0700

    Pass in app to item routes

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import bestbuy from "./routes/bestbuy";
+import item from "./routes/item";
 
 export default () => {
   const app = Router();
 
   bestbuy(app);
+  item(app);
 
   return app;
 };
