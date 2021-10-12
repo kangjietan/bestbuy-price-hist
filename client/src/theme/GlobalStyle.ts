9461bbde902ee6523d3c79de89bc6e1eff259c88
kangjietan <kangjietan@gmail.com>
2021-10-12 00:00:26 -0700

    Add css variables for color scheme

--- client/src/theme/GlobalStyle.ts
+++ client/src/theme/GlobalStyle.ts
@@ -2,6 +2,18 @@ import { createGlobalStyle } from "styled-components";
 import Sanitize from "./sanitize";
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary-color: #182628;
+    --secondary-color: #57BA98;
+    --secondary-variant-color: #3B945E;
+    --bg-color: #65CCB8;
+    --text-color: #F2F2F2;
+  }
+
+  body {
+    background-color: var(--bg-color);
+  }
+
   ${Sanitize}
 `;
 
