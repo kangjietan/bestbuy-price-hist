a38c9577087b503a87363a7b7611ddf307902f8e
kangjietan <kangjietan@gmail.com>
2021-10-11 23:59:33 -0700

    Navbar with styles

---
+++ client/src/components/Navbar/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100px;
+  background-color: var(--bg-color);
+  box-shadow: 0px 10px 10px 1px rgb(0 0 0 / 50%);
+`;
+
+export const Navigation = styled.nav``;
