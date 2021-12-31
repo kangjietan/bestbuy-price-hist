4311cf18823089dfdbc581a061e47cd9ab8b0271
kangjietan <kangjietan@gmail.com>
2021-12-31 15:44:36 -0800

    Wrap App in Provider with Redux store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,4 +4,12 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
