c78094fa13aaf7869e203a788c13cb4aae253862
kangjietan <kangjietan@gmail.com>
2022-01-16 19:24:11 -0800

    Style and toggle transition for nav bar on scroll direction

--- client/src/components/Navbar/styles.ts
+++ client/src/components/Navbar/styles.ts
@@ -1,12 +1,32 @@
 import styled from "styled-components";
 
-export const Header = styled.header`
+interface HeaderProps {
+  direction: string;
+}
+
+export const Header = styled.header<HeaderProps>`
   position: fixed;
   top: 0;
   width: 100%;
+  z-index: 10;
   height: 100px;
   background-color: var(--bg-color);
-  box-shadow: 0px 10px 10px 1px rgb(0 0 0 / 50%);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding: 0px 50px;
+  transition: all 0.4s;
+  transform: ${(props: HeaderProps) =>
+    props.direction === "up" ? "translateY(0)" : "translateY(-100px)"};
+  visibility: ${(props: HeaderProps) =>
+    props.direction === "up" ? "visible" : "hidden"};
 `;
 
-export const Navigation = styled.nav``;
+export const Navigation = styled.nav`
+  display: flex;
+  /* justify-content: space-between; */
+  align-items: center;
+  position: relative;
+  width: 100%;
+  color: var(--text-color);
+  z-index: 11;
+  height: 100px;
+`;
