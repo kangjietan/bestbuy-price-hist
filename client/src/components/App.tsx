65e6e78ec825eee1cbe97b3c5ee36bd75fed2f71
kangjietan <kangjietan@gmail.com>
2022-01-30 20:14:31 -0800

    Fetch list and pass it down

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import Navbar from "./Navbar/Navbar";
+import List from "./List/List";
 
 import GlobalStyle from "../theme/GlobalStyle";
 
-const Container = styled.div``;
+import { useFetchAllItemsQuery } from "../services/item";
 
-const App = () => {
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  min-height: 100vh;
+  margin-top: 100px;
+`;
+
+const App: React.FC = () => {
+  const { data: list = [], error, isLoading } = useFetchAllItemsQuery();
+
   return (
     <Container>
       <GlobalStyle />
       <Navbar></Navbar>
+      <List list={list} />
     </Container>
   );
 };
