ed9e68d4025d4f1a1d4f5856155f7b765c0d857c
kangjietan <kangjietan@gmail.com>
2022-01-30 20:13:46 -0800

    Pagination and styles

--- client/src/components/List/List.tsx
+++ client/src/components/List/List.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ItemCard from "../ItemCard/ItemCard";
 
-import { Container } from "./styles";
+import Stack from "@mui/material/Stack";
+import { Container, Pagination } from "./styles";
 
 import { Item } from "../../interfaces";
 
@@ -11,11 +12,31 @@ interface ListProps {
 }
 
 const List: React.FC<ListProps> = ({ list }) => {
+  const [page, setPage] = useState(1);
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+  const listStart = (page - 1) * 10;
+  const listEnd = page * 10;
+  console.log(listStart, listEnd);
   return (
-    <Container>
-      {list.map((item) => (
-        <ItemCard item={item} />
-      ))}
+    <Container maxWidth="xl">
+      <Stack spacing={3}>
+        {list && (
+          <Pagination
+            size="large"
+            shape="rounded"
+            count={Math.ceil(list.length / 10)}
+            onChange={handlePageChange}
+          />
+        )}
+        {list.slice(listStart, listEnd).map((item) => (
+          <ItemCard item={item} />
+        ))}
+      </Stack>
     </Container>
   );
 };
--- client/src/components/List/styles.ts
+++ client/src/components/List/styles.ts
@@ -1,5 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
+import MuiContainer from "@mui/material/Container";
+import MuiPagination from "@mui/material/Pagination";
 
+export const Container = styled(MuiContainer)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+export const Pagination = styled(MuiPagination)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .MuiPagination-ul {
+    background-color: white;
+    padding: 10px;
+    border-radius: 4px;
+  }
 `;
