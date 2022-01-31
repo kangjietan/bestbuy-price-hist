f3fbb1ed2734547644e28c39b6976c7982a7c1f8
kangjietan <kangjietan@gmail.com>
2022-01-30 20:15:04 -0800

    Item Card and styles

--- client/src/components/ItemCard/ItemCard.tsx
+++ client/src/components/ItemCard/ItemCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { useFetchBestBuyProductInfoQuery } from "../../services/bestBuy";
 import { Item } from "../../interfaces";
-import { Container } from "./styles";
+import { Container, Image } from "./styles";
 
 interface ItemCardProps {
   item: Item;
@@ -11,11 +11,10 @@ interface ItemCardProps {
 const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
   const { sku } = item;
   const { data, error, isLoading } = useFetchBestBuyProductInfoQuery(sku);
-  console.log(data);
   if (data) {
     return (
       <Container>
-        <img src={data.image} />
+        <Image src={data.image} />
       </Container>
     );
   }
--- client/src/components/ItemCard/styles.ts
+++ client/src/components/ItemCard/styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+import Card from "@mui/material/Card";
+
+export const Container = styled(Card)`
+  background-color: var(--secondary-color);
+  padding: 10px;
+`;
+
+export const Image = styled.img`
+  object-fit: cover;
+`;
