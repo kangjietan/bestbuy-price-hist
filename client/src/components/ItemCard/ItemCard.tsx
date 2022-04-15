d036236c40b910e154c5e1e427cd89f2c4b8c0e5
Kang Jie Tan <kangjietan@gmail.com>
2022-04-15 01:19:28 -0700

    Item card and basic styles

--- client/src/components/ItemCard/ItemCard.tsx
+++ client/src/components/ItemCard/ItemCard.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useFetchBestBuyProductInfoQuery } from "../../services/bestBuy";
 import { Item } from "../../interfaces";
-import { Container, Image } from "./styles";
+import { CardContainer, Image, ProductName, Container } from "./styles";
 
 interface ItemCardProps {
   item: Item;
@@ -11,15 +11,43 @@ interface ItemCardProps {
 const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
   const { sku } = item;
   const { data, error, isLoading } = useFetchBestBuyProductInfoQuery(sku);
+  const [hoverShadow, setHoverShadow] = useState(2);
   if (data) {
     return (
       <Container>
-        <Image src={data.image} />
+        <CardContainer
+          sx={{ boxShadow: hoverShadow, bgcolor: "white" }}
+          onMouseEnter={() => setHoverShadow(3)}
+          onMouseLeave={() => setHoverShadow(2)}
+        >
+          <ProductName>{data.name}</ProductName>
+          <Image src={data.image} />
+        </CardContainer>
       </Container>
     );
+    // return data.image ? (
+    //   <Container>
+    //     <CardContainer
+    //       sx={{ boxShadow: hoverShadow, bgcolor: "white" }}
+    //       onMouseEnter={() => setHoverShadow(3)}
+    //       onMouseLeave={() => setHoverShadow(2)}
+    //     >
+    //       <ProductName>{data.name}</ProductName>
+    //       <Image src={data.image} />
+    //     </CardContainer>
+    //   </Container>
+    // ) : (
+    //   <Container>No image for this item. {data.name}</Container>
+    // );
   }
 
-  return <Container>{isLoading}</Container>;
+  return !isLoading ? (
+    <Container>
+      <CardContainer>
+        <p>This item is no longer being sold</p>
+      </CardContainer>
+    </Container>
+  ) : null;
 };
 
 export default ItemCard;
--- client/src/components/ItemCard/styles.ts
+++ client/src/components/ItemCard/styles.ts
@@ -2,11 +2,43 @@ import styled from "styled-components";
 
 import Card from "@mui/material/Card";
 
-export const Container = styled(Card)`
-  background-color: var(--secondary-color);
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 350px;
+  height: 350px;
+  cursor: pointer;
+  transition: all 1s ease-in;
+`;
+
+export const CardContainer = styled(Card)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: all 1s ease-in;
+  width: 100%;
+  height: 100%;
   padding: 10px;
 `;
 
-export const Image = styled.img`
-  object-fit: cover;
+export const ProductName = styled.div`
+  /* height: 0; */
+  /* transform: scaleY(0); */
+  transition: all 0.2s ease-in;
+  margin-bottom: 1rem;
+  text-align: center;
 `;
+
+export const Image = styled.img`
+  object-fit: scale-down;
+  width: 200px;
+  height: 200px;
+  transition: all 0.2s ease-in;
+`;
+
+export const DropDownContainer = styled.div`
+  
+`
