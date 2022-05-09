a53d507950ac2d5bcba7f31533719620a745f755
Kang Jie Tan <kangjietan@gmail.com>
2022-05-08 21:42:58 -0700

    Add Accordion/drop down for price

--- client/src/components/ItemCard/ItemCard.tsx
+++ client/src/components/ItemCard/ItemCard.tsx
@@ -2,52 +2,69 @@ import React, { useState } from "react";
 
 import { useFetchBestBuyProductInfoQuery } from "../../services/bestBuy";
 import { Item } from "../../interfaces";
-import { CardContainer, Image, ProductName, Container } from "./styles";
+import {
+  CardContainer,
+  Image,
+  ProductName,
+  GlassContainer,
+  Loading,
+  ProductPrice,
+  Accordion,
+  ProductNotSoldText,
+} from "./styles";
+import { setCurrentProduct } from "../../features/modal/modalSlice";
+import { useAppDispatch } from "../../app/hooks";
 
 interface ItemCardProps {
   item: Item;
+  openModal: () => void;
 }
 
-const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
+const ItemCard: React.FC<ItemCardProps> = ({ item, openModal }) => {
   const { sku } = item;
+  const dispatch = useAppDispatch();
   const { data, error, isLoading } = useFetchBestBuyProductInfoQuery(sku);
   const [hoverShadow, setHoverShadow] = useState(2);
+  const [showRegularPrice, setShowRegularPrice] = useState(false);
+
   if (data) {
     return (
-      <Container>
-        <CardContainer
-          sx={{ boxShadow: hoverShadow, bgcolor: "white" }}
-          onMouseEnter={() => setHoverShadow(3)}
-          onMouseLeave={() => setHoverShadow(2)}
-        >
-          <ProductName>{data.name}</ProductName>
-          <Image src={data.image} />
-        </CardContainer>
-      </Container>
+      <CardContainer
+        sx={{ boxShadow: hoverShadow, bgcolor: "white" }}
+        onMouseEnter={() => {
+          setHoverShadow(3);
+          setShowRegularPrice(true);
+        }}
+        onMouseLeave={() => {
+          setHoverShadow(1);
+          setShowRegularPrice(false);
+        }}
+        onClick={() => {
+          dispatch(setCurrentProduct(data));
+          openModal();
+        }}
+      >
+        <ProductName>{data.name}</ProductName>
+        <Image src={data.image} />
+        <Accordion expanded={showRegularPrice}>
+          <ProductPrice>{`$${data.salePrice}`}</ProductPrice>
+          <ProductPrice>{`List Price: $${data.regularPrice}`}</ProductPrice>
+        </Accordion>
+      </CardContainer>
     );
-    // return data.image ? (
-    //   <Container>
-    //     <CardContainer
-    //       sx={{ boxShadow: hoverShadow, bgcolor: "white" }}
-    //       onMouseEnter={() => setHoverShadow(3)}
-    //       onMouseLeave={() => setHoverShadow(2)}
-    //     >
-    //       <ProductName>{data.name}</ProductName>
-    //       <Image src={data.image} />
-    //     </CardContainer>
-    //   </Container>
-    // ) : (
-    //   <Container>No image for this item. {data.name}</Container>
-    // );
   }
 
-  return !isLoading ? (
-    <Container>
-      <CardContainer>
-        <p>This item is no longer being sold</p>
-      </CardContainer>
-    </Container>
-  ) : null;
+  return (
+    <CardContainer centerElements>
+      {!isLoading ? (
+        <ProductNotSoldText>
+          This item is no longer being sold.
+        </ProductNotSoldText>
+      ) : (
+        <Loading />
+      )}
+    </CardContainer>
+  );
 };
 
 export default ItemCard;
--- client/src/components/ItemCard/styles.ts
+++ client/src/components/ItemCard/styles.ts
@@ -1,32 +1,34 @@
 import styled from "styled-components";
 
 import Card from "@mui/material/Card";
+import { CircularProgress, Accordion as MuiAccordion, Typography } from "@mui/material";
+
+interface CardContainerProps {
+  centerElements?: boolean;
+}
 
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  width: 350px;
-  height: 350px;
   cursor: pointer;
   transition: all 1s ease-in;
 `;
 
-export const CardContainer = styled(Card)`
+export const CardContainer = styled(Card)<CardContainerProps>`
   display: flex;
-  justify-content: center;
+  justify-content: ${props => props.centerElements ? "center" : "space-between"};
   align-items: center;
   flex-direction: column;
   transition: all 1s ease-in;
-  width: 100%;
-  height: 100%;
+  width: 350px;
+  height: 400px;
   padding: 10px;
+  cursor: pointer;
 `;
 
 export const ProductName = styled.div`
-  /* height: 0; */
-  /* transform: scaleY(0); */
   transition: all 0.2s ease-in;
   margin-bottom: 1rem;
   text-align: center;
@@ -39,6 +41,33 @@ export const Image = styled.img`
   transition: all 0.2s ease-in;
 `;
 
-export const DropDownContainer = styled.div`
-  
-`
+export const ProductPrice = styled(Typography)``;
+
+export const DropDownContainer = styled.div``;
+
+export const Loading = styled(CircularProgress)``;
+
+export const Accordion = styled(MuiAccordion)`
+  text-align: center;
+  padding: 0.5rem;
+`;
+
+export const ProductNotSoldText = styled(Typography)``
+
+export const GlassContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  transition: all 1s ease-in;
+  padding: 10px;
+  cursor: pointer;
+  width: 350px;
+  height: 350px;
+  color: white;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+`;
