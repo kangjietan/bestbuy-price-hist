4aac536cf95e2f525fcaff8219719b3a00ec4de3
Kang Jie Tan <kangjietan@gmail.com>
2022-09-05 00:00:11 -0700

    Add close button styled components and a tag

--- client/src/components/ProductModal/styles.ts
+++ client/src/components/ProductModal/styles.ts
@@ -9,11 +9,59 @@ export const ModalContentContainer = styled(Container)`
   flex-direction: column;
   padding: 1rem;
 `;
+
+export const ModalContentCloseLeftLine = styled.div`
+  height: 4px;
+  width: 25px;
+  position: absolute;
+  margin-top: 24px;
+  background-color: var(--secondary-color);
+  border-radius: 2px;
+  transform: rotate(45deg);
+  transition: all 0.3s ease-in;
+  cursor: pointer;
+`;
+export const ModalContentCloseRightLine = styled.div`
+  height: 4px;
+  width: 25px;
+  position: absolute;
+  margin-top: 24px;
+  background-color: var(--secondary-color);
+  border-radius: 2px;
+  transform: rotate(-45deg);
+  transition: all 0.3s ease-in;
+  cursor: pointer;
+`;
+
+export const ModalContentCloseButtonContainer = styled.div`
+  width: 25px;
+  align-self: start;
+
+  &:hover ${ModalContentCloseLeftLine} {
+    transform: rotate(-45deg);
+    background-color: var(--secondary-variant-color);
+  }
+  &:hover ${ModalContentCloseRightLine} {
+    transform: rotate(45deg);
+    background-color: var(--secondary-variant-color);
+  }
+`;
+
+export const ModalContentHeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 export const ModalContentHeader = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
   gap: 1rem;
+`;
+
+export const ModalContentAnchor = styled.a`
+  text-decoration: none;
+  color: black;
 `;
 
 export const ModalContentImage = styled.img`
@@ -22,12 +70,13 @@ export const ModalContentImage = styled.img`
   object-fit: scale-down;
 `;
 
-export const ModalContentName = styled.p``;
+export const ModalContentName = styled.p`
+  margin: 0;
+  font-weight: 600;
+`;
 
 export const ModalContentGraphContainer = styled.div`
   /* width: 100%; */
-`
-
-export const ModalContentHistoricalsContainer = styled.div`
-  
 `;
+
+export const ModalContentHistoricalsContainer = styled.div``;
