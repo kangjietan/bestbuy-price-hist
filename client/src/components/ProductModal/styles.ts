403eaa7165aa878f9d36647f4c700a8210212480
Kang Jie Tan <kangjietan@gmail.com>
2022-09-04 21:13:44 -0700

    Add styled components for modal

--- client/src/components/ProductModal/styles.ts
+++ client/src/components/ProductModal/styles.ts
@@ -1,9 +1,33 @@
 import styled from "styled-components";
 
-import { Modal as MuiModal } from "@mui/material";
+import { Container } from "@mui/material";
 
-export const Modal = styled(MuiModal)`
+export const ModalContentContainer = styled(Container)`
+  background-color: white;
   display: flex;
-  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  padding: 1rem;
+`;
+export const ModalContentHeader = styled.div`
+  display: flex;
   align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`;
+
+export const ModalContentImage = styled.img`
+  width: 5rem;
+  height: 5rem;
+  object-fit: scale-down;
+`;
+
+export const ModalContentName = styled.p``;
+
+export const ModalContentGraphContainer = styled.div`
+  /* width: 100%; */
+`
+
+export const ModalContentHistoricalsContainer = styled.div`
+  
 `;
